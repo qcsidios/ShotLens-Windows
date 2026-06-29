3cc93328ce97dbbf2c1f0793474439a05a5b311f
青城 <265544699+qcsidios@users.noreply.github.com>
2026-06-29 12:06:00 +0800

    chore: remap Windows release v0.8.7 to v0.1.0

--- ShotLens.Windows/src/ShotLens.Windows.Core/VersionInfo.cs
+++ ShotLens.Windows/src/ShotLens.Windows.Core/VersionInfo.cs
@@ -23,7 +23,7 @@ public static class VersionInfo
             }
         }
 
-        return "v0.8.7";
+        return "v0.1.0";
     }
 
     private static IEnumerable<string> CandidateDirectories()
